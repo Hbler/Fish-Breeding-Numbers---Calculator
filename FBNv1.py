a31d43d2a090cf0130428f658133578d8454a1ce
Hugo Bler <mail.hbler@gmail.com>
2015-09-27 21:17:23 -0300

    Update FBNv1.py
    Current script converted to a function. For further additions in the application.
    Soon the name will also change.

--- FBNv1.py
+++ FBNv1.py
@@ -1,43 +1,30 @@
 # -*- coding: utf-8 -*-
 # Fish Breeding Numbers
 
-# Calculates safe values for the breeding of fish 
+# Calculates safe values for the breeding of fish
 # in tanks equipped with bead filters.
 
-# 1. Request weigh of fish in the tank. 
+# Request weigh of fish in the tank.
 
-fWeigh = input ('Type the weigh of fish in the tank in Lbs (pounds): >> ')
+fWeight = float(raw_input (
+	'Enter the weigh of fish in the tank in Lbs (pounds): '))
 
+def statsCalculator(mass):
+	feedRate = mass * 0.015
+	tSize = feedRate * 200 # tank size.
+	bVolume = feedRate * 0.67 # volume of beads in the filter.
+	flowRate = feedRate * 7.5 # adequate recirculation flow.
+	# Results
+	print
+	print 'Based in this weigh, these are the current values of the breeding:'
+	print '''
+	The feeding rate in this stage is: {: .2f}lbs per day;
+	The tank should be of, at least: {: .2f} gallons;
+	The filter should have at least: {: .2f}ftˆ3 of beads;
+	The recirculation flow should be of: {: .2f}gpm.
+	'''.format(feedRate, tSize, bVolume, flowRate)
+	print '''All these numbers have a safety factor.
+And are calculated for a tank equiped with a bead filter.
+	'''
 
-# 2. Calculates the feeding rate
-
-feedRate = fWeigh * 0.015
-
-
-# 3. Calculates the tank size.
-
-tSize = feedRate * 200
-
-
-# 4. Calculates the necessary volume of beads in the filter.
-
-bVolume = feedRate * 0.67
-
-
-# 5. Calculates the adequate recirculation flow.
-
-flowRate = feedRate * 7.5
-
-# 6. Presents the results in a way that the user can easily identify.
-
-print
-print 'Based in this weigh, these are the current values of the breeding:'
-print
-print 'The feeding rate in this stage is: {: .2f}lbs per day;'.format(feedRate)
-print 'The tank should be of, at least: {: .2f} gallons;'.format(tSize)
-print 'The filter should have at least: {: .2f}ftˆ3 of beads;'.format(bVolume)
-print 'The recirculation flow should be of: {: .2f}gpm.'.format(flowRate)
-print
-print 'All these numbers have a safety factor and are calculated',
-print 'for a tank equiped with a bead filter.'
-print
+statsCalculator(fWeight)
