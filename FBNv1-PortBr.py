689ea243d1127651dc38a29fbf617e8ba567659e
Hugo Bler <mail.hbler@gmail.com>
2015-09-27 21:22:30 -0300

    Update FBNv1-PortBr.py
    Current script translation converted to a function. For further additions in the application.
    Soon the name will also change.

--- FBNv1-PortBr.py
+++ FBNv1-PortBr.py
@@ -1,43 +1,29 @@
 # -*- coding: utf-8 -*-
 # Números para a Criação de Peixes
 
-# Calcula valores seguros para a criação de peixes em tanques equipados com filtros de mídia flutuante.
+# Calcula valores seguros para a criação de peixes
+# em tanques equipados com filtros de mídia flutuante.
 
-# 1. Solicita o peso do total de peixes no tanque.
+fWeight = float(raw_input (
+	'Digite o peso total dos peixes no tanque em Kg(quilos): '))
 
-fWeigh = input ('Digite o peso total dos peixes no tanque em Kg (quilos): >> ')
+def statsCalculator(mass):
+	feedRate = mass * 0.015
+	tSize = feedRate * 757 # tamanho do tanque.
+	bVolume = feedRate * 0.018 # volume necessários de "beads" no filtro.
+	flowRate = feedRate * 1698 # circulação adequada.
+	# Resultados
+	print
+	print 'A partir do peso digitado,',
+	print 'estes são os valores atuais recomendados para a criação:'
+	print '''
+	A taxa de alimentação nesse estágio é de: {: .2f}g por dia;
+	O tanque deve ter pelo menos: {: .2f} litros;
+	O filtro deve ter pelo menos: {: .4f}mˆ3 de mídia flutuante;
+	A circulação de água deve ser de: {: .2f} L/H.
+	'''.format(feedRate, tSize, bVolume, flowRate)
+	print '''Todos os valores tem um fator de segurança.
+E são calculados para um tanque equipado com um filtro de mídias flutuantes.
+	'''
 
-
-# 2. Calcula a taxa de alimentação 
-
-feedRate = fWeigh * 0.015
-
-
-# 3. Calcula o tamanho do tanque.
-
-tSize = feedRate * 757
-
-
-# 4. Calcula o volume necessários de "beads" no filtro.
-
-bVolume = feedRate * 0.018
-
-
-# 5. Calcula a circulação adequada de água no tanque.
-
-flowRate = feedRate * 1698
-
-# 6. Apresenta os resultados de uma maneira que o usuário pode identificar facilmente.
-
-print
-print 'A partir do peso digitado,',
-print 'estes são os valores atuais recomendados para a criação:'
-print
-print 'A taxa de alimentação nesse estágio é de: {: .2f}g por dia;'.format(feedRate)
-print 'O tanque deve ter pelo menos: {: .2f} litros;'.format(tSize)
-print 'O filtro deve ter pelo menos: {: .4f}mˆ3 de mídia flutuante;'.format(bVolume)
-print 'A circulação de água deve ser de: {: .2f} L/H.'.format(flowRate)
-print
-print 'Todos os valores tem um fator de segurança',
-print 'e são calculados para um tanque equipado com um filtro com mídia flutuante.'
-print
+statsCalculator(fWeight)
